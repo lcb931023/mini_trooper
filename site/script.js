9e7872f34ef9eefed8ba02c59980ea70c181d9f7
Michael Nguyen <mln6406@rit.edu>
2014-11-10 09:46:54 -0500

    Added basic framework for game site

---
+++ site/script.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+    $(document).foundation({});
+    /*
+    var iframe = $("#ttIframe");
+    if(iframe != null){
+        iframe.focus();
+    }
+    */
+});
+
+function scrollToElement(selector, time, verticalOffset) {
+    time = typeof(time) != 'undefined' ? time : 1000;
+    verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+    element = $(selector);
+    offset = element.offset();
+    offsetTop = offset.top + verticalOffset;
+    $('html, body').animate({
+        scrollTop: offsetTop
+    }, time);
+}
