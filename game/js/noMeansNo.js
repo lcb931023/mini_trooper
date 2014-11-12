44e02920cca08b63a7933d7a829c80603f281a84
Michael Nguyen <mln6406@rit.edu>
2014-11-11 21:41:35 -0500

    Added pedobear moving to noMeansNo

--- game/js/noMeansNo.js
+++ game/js/noMeansNo.js
@@ -6,28 +6,35 @@ function NoMeansNo (pGameEngine) {
   this.score = 0;
   this.scoreText;
   this.elbin;
+	this.pedobear;
   //Keyboard controls
   this.cursors;
 }
 
 NoMeansNo.prototype.preload = function() {
   //load elbin image
-  this.gameEngine.load.image('logo', 'images/elbin.png');
+  this.gameEngine.load.image('elbin', 'images/elbin.png');
+	this.gameEngine.load.image('pedobear', 'images/pedobear.png');
 };
 
 NoMeansNo.prototype.create = function() {
   //background color
   this.gameEngine.stage.backgroundColor = '#FFF';
 
-  this.elbin = this.gameEngine.add.sprite(50, this.gameEngine.world.centerY, 'logo');
+  this.elbin = this.gameEngine.add.sprite(50, this.gameEngine.world.centerY, 'elbin');
   this.elbin.anchor.setTo(0.5, 0.5);
+	
+	this.pedobear = this.gameEngine.add.sprite(50, this.gameEngine.world.centerY, 'pedobear');
+  this.pedobear.anchor.setTo(0.5, 0.5);
 
-  //make elbins face smaller
+  //scale our sprites
   this.elbin.scale.setTo(0.3, 0.3);
+	this.pedobear.scale.setTo(1,1);
 
-  //enable physics so elbin can move
+  //enable physics so sprites can move
   this.gameEngine.physics.enable(this.elbin, Phaser.Physics.ARCADE);
-
+	this.gameEngine.physics.enable(this.pedobear, Phaser.Physics.ARCADE);
+	
   this.cursors = this.gameEngine.input.keyboard.createCursorKeys();
 
 };
@@ -42,5 +49,7 @@ NoMeansNo.prototype.update = function() {
       //Don't move
       this.elbin.body.velocity.x = 0;
   }
+	
+	this.pedobear.body.velocity.x = 150;
 
 };
