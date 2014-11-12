edc33f32df292db40ca7ea5df6098fff1581245b
Michael Nguyen <mln6406@rit.edu>
2014-11-11 21:55:30 -0500

    Added house and better pedobear

--- game/js/noMeansNo.js
+++ game/js/noMeansNo.js
@@ -7,6 +7,7 @@ function NoMeansNo (pGameEngine) {
   this.scoreText;
   this.elbin;
 	this.pedobear;
+	this.house;
   //Keyboard controls
   this.cursors;
 }
@@ -15,17 +16,21 @@ NoMeansNo.prototype.preload = function() {
   //load elbin image
   this.gameEngine.load.image('elbin', 'images/elbin.png');
 	this.gameEngine.load.image('pedobear', 'images/pedobear.png');
+	this.gameEngine.load.image('house', 'images/house.png');
 };
 
 NoMeansNo.prototype.create = function() {
   //background color
   this.gameEngine.stage.backgroundColor = '#FFF';
 
-  this.elbin = this.gameEngine.add.sprite(50, this.gameEngine.world.centerY, 'elbin');
+  this.elbin = this.gameEngine.add.sprite(150, this.gameEngine.world.centerY, 'elbin');
   this.elbin.anchor.setTo(0.5, 0.5);
 	
-	this.pedobear = this.gameEngine.add.sprite(50, this.gameEngine.world.centerY, 'pedobear');
+	this.pedobear = this.gameEngine.add.sprite(0, this.gameEngine.world.centerY, 'pedobear');
   this.pedobear.anchor.setTo(0.5, 0.5);
+	
+	this.house = this.gameEngine.add.sprite(915, (this.gameEngine.world.centerY - 50), 'house');
+  this.house.anchor.setTo(0.5, 0.5);
 
   //scale our sprites
   this.elbin.scale.setTo(0.3, 0.3);
