657adee5e9844296f4807fa65c67c0d68bc01b6a
John Watson <jjw9765@rit.edu>
2014-11-13 20:33:35 -0500

    Start Text Screen, Selects Random Mini Game and Plays It

--- game/js/main.js
+++ game/js/main.js
@@ -10,22 +10,64 @@ window.onload = function() {
 
   var noMeansNo = new NoMeansNo(gameEngine);
 	var musicMembrane = new MusicMembrane(gameEngine);
+	
+	var titleText = "Mini Trooper";
+	var titleStyle = {font: "70px Arial", fill:"#000", align:"center" };
+	
+	var startText = "Press S to Start";
+	var startStyle = {font: "40px Arial", fill:"#000", align:"center" };
+	
+	 //Keyboard controls
+  var startKey;
+	
+	//ARRAY OF ALL MINI GAMES
+	var miniGames = ["noMeansNo", "musicMembrane", "nyanRace"];
+	
+	//Keeps track of what mini game is randomly selected
+	var currentMiniGame;
 
-  function preload () {
+  //preload all minigames
+	function preload () {
     noMeansNo.preload();
-		musicMembrane.preload();
+	  musicMembrane.preload();
   }
 
   function create () {
-    gameEngine.stage.backgroundColor = '#FFF';
     
-    noMeansNo.create();
-		musicMembrane.create();
+		gameEngine.stage.backgroundColor = '#FFF';
+    
+		var title = gameEngine.add.text(gameEngine.world.centerX - 190, gameEngine.world.centerY - 100, titleText, titleStyle);
+		
+		var start = gameEngine.add.text(gameEngine.world.centerX - 225, gameEngine.world.centerY + 100, startText, startStyle);
+		
+		
+		startKey = gameEngine.input.keyboard.addKey(Phaser.Keyboard.S);
+		startKey.onDown.add(startFunction, this);
   }
+	
+	function startFunction () {
+		
+		//randomly select mini game
+		var randomMiniGame = Math.floor(Math.random() * miniGames.length);
+		
+		//set current MiniGame to currentMiniGame
+		currentMiniGame = miniGames[randomMiniGame];
+
+		/* IF currentMiniGame == "noMeansNo" */
+		currentMiniGame = "noMeansNo";
+		noMeansNo.create();
+		
+		/* IF currentMiniGame == "musicMembrane" etc... */
+		
+		
+	}
 
   function update() {
-    noMeansNo.update();
-		musicMembrane.update();
+		
+		if (currentMiniGame == "noMeansNo"){
+			noMeansNo.update();
+		}
+		
   }
 
 };
