e1b7677f4ed808db2b799251771186c5759e2816
lcb931023 <lcb931023@gmail.com>
2014-11-13 20:39:37 -0500

    minigame outcome state

--- game/js/main.js
+++ game/js/main.js
@@ -10,19 +10,19 @@ window.onload = function() {
 
   var noMeansNo = new NoMeansNo(gameEngine);
 	var musicMembrane = new MusicMembrane(gameEngine);
-	
+
 	var titleText = "Mini Trooper";
 	var titleStyle = {font: "70px Arial", fill:"#000", align:"center" };
-	
+
 	var startText = "Press S to Start";
 	var startStyle = {font: "40px Arial", fill:"#000", align:"center" };
-	
+
 	 //Keyboard controls
   var startKey;
-	
+
 	//ARRAY OF ALL MINI GAMES
 	var miniGames = ["noMeansNo", "musicMembrane", "nyanRace"];
-	
+
 	//Keeps track of what mini game is randomly selected
 	var currentMiniGame;
 
@@ -33,41 +33,46 @@ window.onload = function() {
   }
 
   function create () {
-    
+
 		gameEngine.stage.backgroundColor = '#FFF';
-    
+
 		var title = gameEngine.add.text(gameEngine.world.centerX - 190, gameEngine.world.centerY - 100, titleText, titleStyle);
-		
+
 		var start = gameEngine.add.text(gameEngine.world.centerX - 225, gameEngine.world.centerY + 100, startText, startStyle);
-		
-		
+
+
 		startKey = gameEngine.input.keyboard.addKey(Phaser.Keyboard.S);
 		startKey.onDown.add(startFunction, this);
   }
-	
+
 	function startFunction () {
-		
+
 		//randomly select mini game
 		var randomMiniGame = Math.floor(Math.random() * miniGames.length);
-		
+
 		//set current MiniGame to currentMiniGame
 		currentMiniGame = miniGames[randomMiniGame];
 
 		/* IF currentMiniGame == "noMeansNo" */
 		currentMiniGame = "noMeansNo";
 		noMeansNo.create();
-		
+
 		/* IF currentMiniGame == "musicMembrane" etc... */
-		
-		
+
+
 	}
 
   function update() {
-		
+
 		if (currentMiniGame == "noMeansNo"){
 			noMeansNo.update();
+      if (noMeansNo.outcome == 1) {
+        // something happen after win
+      } else if (noMeasNo.outcome == -1) {
+        // something happen after lose
+      }
 		}
-		
+
   }
 
 };
--- game/js/noMeansNo.js
+++ game/js/noMeansNo.js
@@ -5,6 +5,9 @@ NoMeansNo.prototype.constructor = NoMeansNo;
 
 function NoMeansNo (pGameEngine) {
   this.gameEngine = pGameEngine;
+  // [TODO] enum
+  // 0 = ongoing, 1 = won, -1 = lost
+  this.outcome = 0;
 
   this.score = 0;
   this.scoreText;
@@ -22,18 +25,18 @@ NoMeansNo.prototype.preload = function() {
   this.gameEngine.load.image('elbin', 'images/elbin.png');
 	this.gameEngine.load.image('house', 'images/house.png');
 	this.gameEngine.load.image('background', 'images/noMeansNoBG.png');
-	
+
 	this.gameEngine.load.image('pedobear', 'images/pedobear_sprite.png', 26, 55);
 	this.gameEngine.load.spritesheet('button', 'images/button_sprite.png', 630,125);
 };
 
 NoMeansNo.prototype.create = function() {
-		
+
 	this.gameEngine.add.sprite(0, 0, 'background');
-	
+
 	//button actions event handlers
 	this.runBtn = this.gameEngine.add.button(this.gameEngine.world.centerX - 315, 400, 'button', actionOnClick, this, 1, 0);
-	
+
   this.elbin = this.gameEngine.add.sprite(150, this.gameEngine.world.centerY + 65, 'elbin');
   this.elbin.anchor.setTo(0.5, 0.5);
 
@@ -50,10 +53,10 @@ NoMeansNo.prototype.create = function() {
   this.gameEngine.physics.enable(this.elbin, Phaser.Physics.ARCADE);
 	this.gameEngine.physics.enable(this.pedobear, Phaser.Physics.ARCADE);
 	this.gameEngine.physics.enable(this.house, Phaser.Physics.ARCADE);
-	
+
   //Stop the following keys from propagating up to the browser
   this.gameEngine.input.keyboard.addKeyCapture([ Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT, Phaser.Keyboard.SPACEBAR ]);
-	
+
 	//CONSTANT
 	this.elbin.body.velocity.x = 50;
 };
@@ -70,10 +73,12 @@ NoMeansNo.prototype.update = function() {
 		console.log("Elbin is dead");
 		// Removes elbin from the screen
 		this.elbin.kill();
+    this.outcome = -1;
 	}
 
 	function liveElbin (elbin, house) {
 		console.log("Elbin is alive");
+    this.outcome = 1;
 	}
 
 };
