5375d81e6ccb1af524736123125594209193aa6d
lcb931023 <lcb931023@gmail.com>
2014-11-13 23:48:22 -0500

    winscreen goes to next after couple seconds

--- game/js/winScreen.js
+++ game/js/winScreen.js
@@ -10,10 +10,12 @@ function WinScreen (pGameEngine) {
 
 WinScreen.prototype.preload = function() {
   // Load image assets
+  this.gameEngine.load.image('nextBG', 'images/nextBg.png');
 };
 
 WinScreen.prototype.create = function() {
-
+  this.bg = this.gameEngine.add.sprite(0,0,'nextBG');
+  this.gameEngine.time.events.add(Phaser.Timer.SECOND * 1.5, function f(){this.done = true;}, this);
 };
 
 WinScreen.prototype.update = function() {
@@ -22,4 +24,5 @@ WinScreen.prototype.update = function() {
 
 WinScreen.prototype.destroy = function() {
   Screen.prototype.destroy.call(this);
+  this.bg.destroy();
 };
