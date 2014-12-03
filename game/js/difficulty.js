086d4fbc26992cf4f09b15d13af2a8b7f077e547
lcb931023 <lcb931023@gmail.com>
2014-12-02 21:44:19 -0500

    difficulty is functional

--- game/js/difficulty.js
+++ game/js/difficulty.js
@@ -1,25 +1,41 @@
+"use strict";
+
 var DIFFICULTY = {
-	bh: {
-    turretAmount: [
+	current : 0
+	,	bh: {
+		turretAmt: [
       1,
       2,
       3,
       3,
-      4,
-      4,
-      5,
-      5,
-      6,
-      6,
-      6,
-      6,
-      6,
-      6,
-      6,
-    ],
-    fireRate: [
+      // 4,
+      // 4,
+      // 5,
+      // 5,
+      // 6,
+      // 6,
+      // 6,
+      // 6,
+      // 6,
+      // 6,
+      // 6,
+    ]
+    , fireRate: [
+			400,
+			400,
+			500,
+			400,
+			500,
+			400,
+			500,
+			400,
 
-    ],
-  },
+    ]
+  }
+	,	get: function(gameName, varName) {
+		// Get difficulty;
+		// if already highest, return highest
+		return this[gameName][varName][this.current] ? this[gameName][varName][this.current] : this[gameName][varName][ this[gameName][varName].length-1 ];
+	}
 
 };
