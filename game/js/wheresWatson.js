dc30ba65477e94f3ef565c8b021f79375b793d05
John Watson <jjw9765@rit.edu>
2014-12-04 22:02:04 -0500

    Wheres Watson Complete

--- game/js/wheresWatson.js
+++ game/js/wheresWatson.js
@@ -11,6 +11,7 @@ function WheresWatson (pGameEngine) {
   this.outcome = 0;
   this.score = 0;
   this.scoreText;
+	this.bg;
 
 	this.counterTitle;
 	this.timer;
@@ -20,13 +21,26 @@ function WheresWatson (pGameEngine) {
 	this.counterStyle = {font: "70px ChickenButt", fill:"#fff", align:"center" };
 
 	this.instructions;
-	this.instructionsTxt = "Wait for nyan Cat and then choose the right color!";
+	this.instructionsTxt = "Find JJ among the illegal aliens!";
 	this.instructionsStyle = {font: "30px ChickenButt", fill:"#000", align:"center" };
 
 	this.difficulty;
 	this.difficultyStyle = {font: "50px ChickenButt", fill:"#fff", align:"center" };
 	
 	this.watson;
+	this.people = [];
+	
+	this.nonWhites = [
+		"Changbai",
+		"Elbin",
+		"Mike"
+	];
+	
+	this.numFaces;
+	this.faceSize;
+	this.randomX;
+	this.randomY;
+	
 }
 
 WheresWatson.prototype.preload = function() {
@@ -35,9 +49,9 @@ WheresWatson.prototype.preload = function() {
 	this.gameEngine.load.image('Changbai', 'images/changbai.png');
 	this.gameEngine.load.image('Elbin', 'images/elbin.png');
 	this.gameEngine.load.image('Mike', 'images/mike.png');
-	this.gameEngine.load.image('background1', 'images/nyanCat_Red.png');
-	this.gameEngine.load.image('background2', 'images/nyanCat_Violet.png');
-	this.gameEngine.load.image('background3', 'images/nyanCat_Yellow.png');
+	this.gameEngine.load.image('background1', 'images/watsonBG1.png');
+	this.gameEngine.load.image('background2', 'images/watsonBG2.png');
+	this.gameEngine.load.image('background3', 'images/watsonBG3.png');
 };
 
 WheresWatson.prototype.create = function() {
@@ -54,19 +68,39 @@ WheresWatson.prototype.gameStart = function() {
 
 	//get rid of instructions
 	this.instructions.destroy();
-
-	this.gameEngine.stage.backgroundColor = '#0F4D8F';
-	this.counterTitle = this.gameEngine.add.text(this.gameEngine.world.centerX, this.gameEngine.world.centerY, this.counter, this.counterStyle);
-
-	this.difficultyTxt = "Difficulty: " + DIFFICULTY.nr.current;
-	this.difficulty = this.gameEngine.add.text(60, 30, this.difficultyTxt, this.difficultyStyle);
 	
-	this.watson = this.gameEngine.add.sprite(100, 100, "JJ");
+	var randomBackground = parseInt(Math.random() * 3) + 1;
+
+	this.bg = this.gameEngine.add.sprite(0, 0, 'background' + randomBackground);
+	
+	this.randomX = parseInt(Math.random() * 960);
+	this.randomY = parseInt(Math.random() * 560);
+	
+	this.numFaces = DIFFICULTY.get(this.gameId, "numFaces");
+	this.faceSize = DIFFICULTY.get(this.gameId, "faceSize");
+	
+	this.watson = this.gameEngine.add.sprite(this.randomX, this.randomY, "JJ");
 	this.watson.anchor.set(0.5);
 	this.watson.inputEnabled = true;
 	this.watson.events.onInputDown.add(pressWatson,this);
 	this.watson.anchor.setTo(0.5, 0.5);
-	this.watson.scale.setTo(0.3, 0.3);
+	this.watson.scale.setTo(this.faceSize, this.faceSize);
+	
+	for(var i = 0; i < this.numFaces; i++){
+		
+		this.randomX = parseInt(Math.random() * 960);
+	  this.randomY = parseInt(Math.random() * 560);
+		var rand = parseInt(Math.random() * this.nonWhites.length);
+		this.people[i] = this.gameEngine.add.sprite(this.randomX, this.randomY, this.nonWhites[rand]);
+		this.people[i].anchor.setTo(0.5, 0.5);
+		this.people[i].scale.setTo(this.faceSize, this.faceSize);
+		
+	}
+	
+	this.counterTitle = this.gameEngine.add.text(this.gameEngine.world.centerX, this.gameEngine.world.centerY, this.counter, this.counterStyle);
+
+	this.difficultyTxt = "Difficulty: " + DIFFICULTY.ww.current;
+	this.difficulty = this.gameEngine.add.text(60, 30, this.difficultyTxt, this.difficultyStyle);
 
 	
 
@@ -86,7 +120,7 @@ WheresWatson.prototype.countdown = function() {
 	if(this.counter > 0){
 		this.counterTitle = this.gameEngine.add.text(this.gameEngine.world.centerX, this.gameEngine.world.centerY, this.counter, this.counterStyle);
 	} else {
-
+		this.outcome = -1;
 	}
 
 };
@@ -112,18 +146,15 @@ WheresWatson.prototype.destroy = function() {
   // Detach listeners
   // No listeners attached in this game, other than the button, which gets its listener destroyed with itself
   // Remove elements
-  this.nyanCat.destroy();
+	this.watson.destroy();
+	this.bg.destroy();
   this.counterTitle.destroy();
-	this.nyanCatMusic.destroy();
 	this.difficulty.destroy();
   this.timer.destroy();
-	this.moveNyanCat = false;
 	this.counter = 5;
 	this.counterStyle = {font: "70px ChickenButt", fill:"#000", align:"center" };
-	for (var i = 0; i < this.buttons.length; i++) {
-		this.buttons[i].destroy();
+	for (var i = 0; i < this.people.length; i++) {
+		this.people[i].destroy();
 	}
-	this.totalStars = 0;
-	this.nyanCatStars.destroy();
 	this.gameStarted = false;
 }
